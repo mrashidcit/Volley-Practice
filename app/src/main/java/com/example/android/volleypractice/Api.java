574efb5c4552c4cb269c900b070846e216a70ae2
Rashid Saleem <mrashidcit@gmail.com>
2018-02-16 18:04:45 +0500

    Standard Request Json Request

---
+++ app/src/main/java/com/example/android/volleypractice/Api.java
@@ -0,0 +1,17 @@
+package com.example.android.volleypractice;
+
+/**
+ * Created by Rashid Saleem on 16-Feb-18.
+ */
+
+public class Api {
+
+
+    private Api() {
+    }
+
+
+    public static  final String URL = "https://api.sandbox.paypal.com";
+
+
+}
--- app/src/main/java/com/example/android/volleypractice/MainActivity.java
+++ app/src/main/java/com/example/android/volleypractice/MainActivity.java
@@ -8,8 +8,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
+    private JsonObjectRequest mJsonObjectRequest;
     private String mUrl;
 
     @Override
@@ -25,10 +29,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mResponseTextView = (TextView) findViewById(R.id.tv_response);
-        mUrl = "http://www.google.com";
+//        mUrl = "http://www.google.com";
+        mUrl = "http://echo.jsontest.com/key/value/one/two";
 
         // Initialize the RequestQueue.
         mRequestQueue = Volley.newRequestQueue(this);
+
+        mJsonObjectRequest = new JsonObjectRequest(Request.Method.GET, mUrl, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mResponseTextView.setText("Response: " + response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mResponseTextView.setText("That didn't work!");
+            }
+
+        });
+
+
+
+
 
 
         mStringRequest = new StringRequest(Request.Method.GET, mUrl,
@@ -53,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mResponseTextView.setText("Loading....");
-        mRequestQueue.add(mStringRequest);
+//        mRequestQueue.add(mStringRequest);
+        mRequestQueue.add(mJsonObjectRequest);
 
 
 
